88614ed483e235318d24e714cfb61abee3468765
Roberto Teixeira <robertoteixeira.developer@gmail.com>
2022-06-07 13:15:40 -0300

    first commit

---
+++ OOP-SOLID/Class.php
@@ -0,0 +1,32 @@
+<?php
+// php Class.php
+echo "hello";
+/*
+    Definition of Class
+*/
+
+class Project {
+
+    //PROPERTIES - Caractaristics 
+
+    public $title;
+
+    public $description;
+
+    //METHODS - Behavior
+    public function Talk($user){
+
+    }
+}
+
+// OBJECTS - Insances of a Class
+
+
+
+
+
+
+
+
+
+?>
